5656a207c56ff723166c4a33f1f37a32399105e9
hisui <ruru.apple.1212@gmail.com>
2023-02-24 18:55:38 +0900

    動的ルーティング

---
+++ next_app/src/pages/router/[name]/setting.js
@@ -0,0 +1,4 @@
+// /router/****/setting でアクセス可能
+export default function Setting() {
+  return <h1>[name]/setting.js</h1>
+}
---
+++ next_app/src/pages/router/blog/[number].js
@@ -0,0 +1,4 @@
+// /router/blog/*** */ でアクセス可能
+export default function Number() {
+  return <h1>[number].js</h1>;
+}
