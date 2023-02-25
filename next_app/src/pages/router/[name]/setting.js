d176b5383f48af5489781eb9bc89c0baa1d40a65
hisui <ruru.apple.1212@gmail.com>
2023-02-25 14:05:07 +0900

    指定されたパスの値を取得

--- next_app/src/pages/router/[name]/setting.js
+++ next_app/src/pages/router/[name]/setting.js
@@ -1,4 +1,25 @@
+import { useRouter } from "next/router";
+
+
 // /router/****/setting でアクセス可能
-export default function Setting() {
-  return <h1>[name]/setting.js</h1>
+export default function Setting({ query }) {
+  // 2. RouterからuseRouterを使用する
+  const router = useRouter()
+  console.log(router)
+
+  return <h1>routerから取得:{router.query.name}</h1>
+
+  // return <h1>{query.name}</h1>;
+}
+
+// URLから引数をもらう
+// 1. getSeverSideProps という関数を使用する
+// contextのqueryというプロパティに入っている
+// SSRを使用する際に使用する
+export async function getServerSideProps(context) {
+
+  const {query} = context
+  return {
+    props: { query },
+  };
 }
