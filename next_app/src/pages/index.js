d7e2e17fc6d94049371b301ef06d8f44f744958e
hisui <ruru.apple.1212@gmail.com>
2023-02-25 18:24:06 +0900

    Linkコンポーネント

--- next_app/src/pages/index.js
+++ next_app/src/pages/index.js
@@ -1,4 +1,22 @@
+import Link from "next/link";
+
 // export default したものが表示される
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <>
+      <h1>Home</h1>
+      {/* Linkコンポーネント */}
+      {/* href属性で遷移先を指定して、aタグを中に書く */}
+      {/* as属性：ダミーのURLを記述可能 */}
+      {/* 遷移時画面のリロードが発生しない */}
+      {/* <Link href="/router" as="/dummy-url"> */}
+      {/* query属性：クエリパラメータの指定可能 */}
+      <Link href={{pathname:"/router", as: "/dummy-url", query: {key: "value"}}}>
+        /router
+      </Link>
+      <br />
+      {/* 画面遷移時リロードが発生する */}
+      <a href="/router"></a>
+    </>
+  );
 }
