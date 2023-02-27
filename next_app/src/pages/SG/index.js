f3ac2842796218c67dee0ad8921fba0978184c9c
hisui <ruru.apple.1212@gmail.com>
2023-02-27 23:53:25 +0900

    SG_getStaticProps

--- next_app/src/pages/SG/index.js
+++ next_app/src/pages/SG/index.js
@@ -1,3 +1,13 @@
-export default function IndexPage() {
-  return <h3>SG</h3>;
+export default function IndexPage({ message }) {
+  return <h3>SG:{message}</h3>;
+}
+
+// SGを使用する際のpropsの渡し方
+// ビルドのタイミングで実行される
+// ここで返す値をビルド時にjsonファイルにしてexportする
+export async function getStaticProps() {
+  console.log("getStaticProps");
+  return {
+    props: { message: "From Static Props" },
+  };
 }
